5cf3c225a03585f636084a08dc3e6883a614fd16
R Moruju <morujurazvan@gmail.com>
2023-01-07 11:30:57 +0000

    Add 'read' and 'create' functionality

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Items
+from .forms import ItemsForm
 # Create your views here.
 
 
 def get_todo_list(request):
-    return render(request, "todo/todo_list.html")
+    items = Items.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, "todo/todo_list.html", context)
+
+
+def add_item(request):
+    if request.method == "POST":
+        form = ItemsForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("get_todo_list")
+
+    form = ItemsForm
+    context = {
+        'form': form
+    }
+    return render(request, 'todo/add_item.html', context)
